e17b5120ccea9e6594f0d5e6c9384bf73a2d972e
munstein <eduardo.munstein@live.com>
2020-04-20 16:37:17 -0300

    Add viewmodel methods.

--- app/src/main/java/com/munstein/gamemanager/base/Resource.kt
+++ app/src/main/java/com/munstein/gamemanager/base/Resource.kt
@@ -5,16 +5,19 @@ class Resource<out T> constructor(
         val data: T?,
         val error: Throwable?
 ) {
-    fun <T> success(data: T): Resource<T> {
-        return Resource(ResourceState.SUCCESS, data, null)
-    }
 
-    fun <T> error(error: Throwable?): Resource<T> {
-        return Resource(ResourceState.ERROR, null, error)
-    }
+    companion object {
+        fun <T> success(data: T): Resource<T> {
+            return Resource(ResourceState.SUCCESS, data, null)
+        }
 
-    fun <T> loading(): Resource<T> {
-        return Resource(ResourceState.LOADING, null, null)
+        fun <T> error(error: Throwable?): Resource<T> {
+            return Resource(ResourceState.ERROR, null, error)
+        }
+
+        fun <T> loading(): Resource<T> {
+            return Resource(ResourceState.LOADING, null, null)
+        }
     }
 }
 
--- app/src/main/java/com/munstein/gamemanager/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/munstein/gamemanager/viewmodels/HomeViewModel.kt
@@ -2,8 +2,50 @@ package com.munstein.gamemanager.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import com.munstein.gamemanager.base.CoroutineViewModel
+import com.munstein.gamemanager.base.Resource
 import com.munstein.gamemanager.interactor.platform.IPlatformInteractor
+import com.munstein.gamemanager.model.Platform
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
-class HomeViewModel(private val platformInteractor: IPlatformInteractor) : CoroutineViewModel() {}
+class HomeViewModel(private val platformInteractor: IPlatformInteractor) : CoroutineViewModel() {
+
+    val insert by lazy { MutableLiveData<Resource<Unit>>() }
+    val remove by lazy { MutableLiveData<Resource<Unit>>() }
+    val platforms by lazy { MutableLiveData<Resource<List<Platform>>>() }
+
+    suspend fun insertPlatform(name: String) {
+        jobs add launch {
+            try {
+                insert.postValue(Resource.loading())
+                platformInteractor.addPlatform(name)
+                insert.postValue(Resource.success(Unit))
+            } catch (x: Exception) {
+                insert.postValue(Resource.error(x))
+            }
+        }
+    }
+
+    suspend fun removePlatform(name: String) {
+        jobs add launch {
+            try {
+                remove.postValue(Resource.loading())
+                platformInteractor.removePlatform(name)
+                remove.postValue(Resource.success(Unit))
+            } catch (x: Exception) {
+                insert.postValue(Resource.error(x))
+            }
+        }
+    }
+
+    suspend fun getPlatforms() {
+        jobs add launch {
+            try {
+                platforms.postValue(Resource.loading())
+                val result = platformInteractor.getPlatforms().await()
+                platforms.postValue(Resource.success(result))
+            } catch (x: Exception) {
+                platforms.postValue(Resource.error(x))
+            }
+        }
+    }
+}
