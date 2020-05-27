acae1af28066310317740ee388b88f9f19ab262d
munstein <eduardo.munstein@live.com>
2020-05-27 17:23:54 -0300

    Implements interactor and repository for games.

---
+++ app/src/main/java/com/munstein/gamemanager/interactor/games/GamesInteractor.kt
@@ -0,0 +1,45 @@
+package com.munstein.gamemanager.interactor.games
+
+import com.munstein.gamemanager.model.Games
+import com.munstein.gamemanager.repository.games.IGamesRepository
+
+class GamesInteractor(private val repository: IGamesRepository) : IGamesInteractor {
+    override suspend fun getGames(platformTitle: String): Games {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteWantGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.want.remove(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    override suspend fun deletePlayingGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.playing.remove(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    override suspend fun deleteHaveGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.have.remove(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    override suspend fun addWantGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.want.add(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    override suspend fun addPlayingGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.playing.add(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    override suspend fun addHaveGame(games: Games, gameTitle: String, platformTitle: String) {
+        games.have.add(gameTitle)
+        setGames(platformTitle, games)
+    }
+
+    private suspend fun setGames(platformTitle: String, games: Games) {
+        repository.setGames(platformTitle, games)
+    }
+
+}
--- app/src/main/java/com/munstein/gamemanager/model/Games.kt
+++ app/src/main/java/com/munstein/gamemanager/model/Games.kt
@@ -1,5 +1,5 @@
 package com.munstein.gamemanager.model
 
-data class Games(val have: List<String> = listOf(),
-                 val want: List<String> = listOf(),
-                 val playing: List<String> = listOf())
+data class Games(val have: ArrayList<String> = arrayListOf(),
+                 val want: ArrayList<String> = arrayListOf(),
+                 val playing: ArrayList<String> = arrayListOf())
